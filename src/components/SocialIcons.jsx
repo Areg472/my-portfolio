245627def598704d3fb2a9fc997f5fc65b3afd90
Aregus <74340447+Areg472@users.noreply.github.com>
2024-11-27 10:07:11 +0400

    social icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,5 +1,5 @@
 import "./SocialIcons.css"
-import {FaReddit, FaTwitter, FaYoutube} from "react-icons/fa";
+import {FaReddit, FaTwitter, FaYoutube, FaGithub} from "react-icons/fa";
 import {FaBluesky, FaDiscord} from "react-icons/fa6";
 
 export function SocialIcons() {
@@ -10,6 +10,7 @@ export function SocialIcons() {
             <a href="https://x.com/Aregtheeditor" target="_blank"><FaTwitter/></a>
             <a href="https://bsky.app/profile/aregus.me" target="_blank"><FaBluesky/></a>
             <a href="https://discord.gg/EZUW8wW5S8" target="_blank"><FaDiscord/></a>
+            <a href="https://github.com/Areg472" target="_blank"><FaGithub/></a>
         </div>
     )
 }
