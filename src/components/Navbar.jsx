28e567cf37c670bb770a9e6fa3ca0f1536cb8337
Aregus <74340447+Areg472@users.noreply.github.com>
2024-12-28 11:13:20 +0400

    the fun update fix 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export function Navbar() {
     return (
         <>
             <div className="flex flex-col md:flex-row justify-center md:space-x-10 space-y-10 md:space-y-0">
-                <motion.div initial={{y: -100}}
+                <motion.div initial={{y: -120}}
                             transition={{type: "spring", duration: 1.5, bounce: 0.55, delay: 1}} animate={{y: 0}}>
                     <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
                         <Link to={"/"}>
@@ -13,7 +13,7 @@ export function Navbar() {
                         </Link>
                     </motion.div>
                 </motion.div>
-                <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} initial={{y: -100}}
+                <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} initial={{y: -120}}
                             transition={{type: "spring", duration: 2, bounce: 0.6, delay: 1}} animate={{y: 0}}>
                     <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
                         <Link to={"/Work"}>
