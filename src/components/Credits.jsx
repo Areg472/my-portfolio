76d5950086e5e47d4155e0c191926f72f58951e5
Aregus <74340447+Areg472@users.noreply.github.com>
2025-08-22 09:37:35 +0400

    credit change

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -3,8 +3,8 @@ import "./Credits.css";
 export function Credits() {
   return (
     <>
-      <a className="hmm" href="https://github.com/Pirkisek" target="_blank">
-        <p className="credit">Some code by @pirkisek</p>
+      <a className="hmm" href="https://github.com/re1exe" target="_blank">
+        <p className="credit">Some code by @Re1</p>
       </a>
     </>
   );
