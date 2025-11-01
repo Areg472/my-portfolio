19980d85c5a4b52d32b2f09252220be6db380834
Aregus <74340447+Areg472@users.noreply.github.com>
2025-11-01 12:43:01 +0400

    image fix

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -11,6 +11,7 @@ export function Work() {
         title={"Areg - Work"}
         description="My smol OwO work!"
         name="Areg"
+        image="https://utfs.io/f/thKihuQxhYcPw3n5zcEF1bloKXeA0d3pP7RDCmGxkgNhTjMa"
       />
       <motion.div
         initial={{ rotate: isMobile ? 0 : 150, scale: 0, opacity: 0 }}
